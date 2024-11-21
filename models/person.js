ed2f80ec02ce0382c7190a97ef1578588edb2f70
jjunlob074 <jjunlob074@g.educaand.es>
2024-11-21 19:41:47 +0100

    uibuild

--- models/person.js
+++ models/person.js
@@ -6,8 +6,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then(result => {
-    console.log(result)
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
